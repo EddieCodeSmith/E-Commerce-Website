3c12a36b3ad4257615b5044a41e10f574e234015
Top G <salamiedwin8888@gmail.com>
2024-07-24 11:39:50 +0100

    Change default DB from sqlite3 to postgresql

--- commerce/settings.py
+++ commerce/settings.py
@@ -1,8 +1,12 @@
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# LOADING ENVIRONMENTAL VARIABLES
+load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -78,8 +82,16 @@ WSGI_APPLICATION = 'commerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'railway',
+        'USER':'postgres',
+        'PASSWORD':os.environ.get('PG_DB_PASS'),
+        'HOST':'roundhouse.proxy.rlwy.net',
+        'PORT':'23199',
+
+
     }
 }
 
